202cbf87b5a461fc2b08c8241fd51b7e328123fd
M.Chaitanya Sai <marvelchaitanya@gmail.com>
2024-11-05 18:13:33 +0530

    Update Kilograms-and-Pounds.py

--- Kilograms-and-Pounds.py
+++ Kilograms-and-Pounds.py
@@ -4,10 +4,12 @@ unit = input("Kilograms or Pounds? (K or P): ")
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs."
+    print(f"Your weight is: {round (weight, 1)} {unit}")
 elif unit == "P":
     weight = weight / 2.205
     unit = "Kgs."
+    print(f"Your weight is: {round (weight, 1)} {unit}")
 else:
     print(f"{unit} is not valid.")
 
-print(f"Your weight is: {weight} {unit}")
+
